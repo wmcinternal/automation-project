a0304811ae9fcbce6f1fab7610c249ca53fbaf4c
wmcintern <137027006+wmcinternal@users.noreply.github.com>
2026-07-15 15:56:10 +0800

    Add files via upload

--- server.py
+++ server.py
@@ -331,9 +331,9 @@ def download_webscrap_source():
     
     webscrap_stream.seek(0)
 
-    return send_file={
+    return send_file=(
         webscrap_stream, mimetype="attachments/zip", as_attachment=True, download_name=f"webscrap_source_{ref_id}.zip"
-    }
+    )
     
 
 
